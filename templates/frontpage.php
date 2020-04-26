a4f51e5324590d78f969c6bfff3cb10a76ee7ec1
vancouver29 <vancouver2990@gmail.com>
2020-04-26 14:30:11 +0200

    fixed index.php

--- templates/frontpage.php
+++ templates/frontpage.php
@@ -14,7 +14,7 @@
                         <div class="topic-info">
                             <a href="topics.php?category=<?php echo urlFormat($topic->category_id); ?>"><?php echo $topic->name; ?></a> >>
                             <a href="topics.php?user=<?php echo urlFormat($topic->user_id); ?>"><?php echo $topic->username; ?></a> >>
-                            <?php echo formateDate($topic->create_date) ; ?>
+                            <?php echo formatDate($topic->create_date) ; ?>
                             <span class="badge float-right"><?php replyCount($topic->id); ?></span>
                         </div>
                     </div>
