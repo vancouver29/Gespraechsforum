03db234ea3b344349c0e36588b93543a003ac0e9
vancouver29 <vancouver2990@gmail.com>
2020-04-29 12:21:10 +0200

    sticky footer

--- templates/includes/footer.php
+++ templates/includes/footer.php
@@ -51,12 +51,15 @@
 
 </main><!-- /.container -->
 
-<footer id="sticky-footer" class="py-4 bg-dark text-white-50 ">
-    <div class="container text-center">
-        <small>Copyright &copy; 2020 TalkingRoom-Vancouver299</small>
+<!-- Footer -->
+<footer class="footer mt-auto py-3">
+    <!-- Copyright -->
+    <div class="container text-center ">
+       <span class="text-muted">© 2020 Copyright: TalkingRoom-Vancouver299</span>
     </div>
+    <!-- Copyright -->
 </footer>
-
+<!-- Footer -->
 
 </body>
 </html>
